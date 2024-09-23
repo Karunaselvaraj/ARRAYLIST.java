2b7ce99fb6f980c5f00bc37c41ed461b76880514
Karunaselvaraj <159015527+Karunaselvaraj@users.noreply.github.com>
2024-09-23 11:53:44 +0530

    Update ARRAYLIST.java

--- ARRAYLIST.java
+++ ARRAYLIST.java
@@ -1,1 +1,25 @@
-
+import java.util.*;
+public class arraylistex1 {
+    public static void main(String[] args){
+        ArrayList<Integer> l=new ArrayList<>();
+        ArrayList<Integer> l1=new ArrayList<>();
+        Integer[] s=new Integer[10];
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        l.add(6);
+        l.add(7);
+        l.add(8);
+        l.add(9);
+        l.add(10);
+        for(int a:l){
+            a=a*a;
+            if(a>30){
+                l1.add(a);
+                System.out.println(a);
+            }
+        }
+    }
+}
